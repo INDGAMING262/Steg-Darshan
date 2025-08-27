9d3b3e9e3fffb10a8342282d680c0d08d6458f6a
INDGAMING262 <128442924+INDGAMING262@users.noreply.github.com>
2025-08-28 01:20:24 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,15 +3,13 @@ set -e
 
 echo "[*] Installing Stego-Darshan..."
 
-
+# Install dependencies via apt
 sudo apt update
 sudo apt install -y python3-pil python3-cryptography
 
-
+# Copy script to /usr/bin
 sudo cp stego-darshan.py /usr/bin/stego-darshan
 sudo chmod +x /usr/bin/stego-darshan
-sudo chmod +x uninstaller.sh
-
 
 echo "[+] Installation complete!"
 echo "Now you can run the tool from anywhere with:"
